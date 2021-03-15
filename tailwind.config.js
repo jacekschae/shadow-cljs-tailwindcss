d94a3645a1e8ee3a832c78f001b0439638ba885a
Jacek Schae <jacek.schae@gmail.com>
2021-03-15 22:37:10 +0100

    Update config; read from output files, update scripts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   purge: {
-    content: ["./src/main/**/*.cljs"]
+    // in prod look at shadow-cljs output file in dev look at runtime, which will change files that are actually compiled; postcss watch should be a whole lot faster
+    content: process.env.NODE_ENV == 'production' ? ["./public/js/main.js"] : ["./public/js/cljs-runtime/*.js"]
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
