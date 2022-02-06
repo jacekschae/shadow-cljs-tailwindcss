021764250c68c0e43a254724ff5d96d0160bb02b
Jacek Schae <jacek.schae@gmail.com>
2022-02-06 16:52:28 +0100

    Update to tailwind-css 3.0 config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  mode: 'jit',
-  purge: {
-    // in prod look at shadow-cljs output file in dev look at runtime, which will change files that are actually compiled; postcss watch should be a whole lot faster
-    content: process.env.NODE_ENV == 'production' ? ["./public/js/main.js"] : ["./public/js/cljs-runtime/*.js"]
-  },
-  darkMode: false, // or 'media' or 'class'
+  // in prod look at shadow-cljs output file in dev look at runtime, which will change files that are actually compiled; postcss watch should be a whole lot faster
+  content: process.env.NODE_ENV == 'production' ? ["./public/js/main.js"] : ["./public/js/cljs-runtime/*.js"],
   theme: {
     extend: {
       fontFamily: {
@@ -14,7 +10,6 @@ module.exports = {
       },
     },
   },
-  variants: {},
   plugins: [
     require('@tailwindcss/forms'),
   ],
