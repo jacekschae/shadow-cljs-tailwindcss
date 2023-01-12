d44a771e7706bbea761379ff60751f9bd8fcff49
Jacob Emcken <jacob@emcken.dk>
2023-01-12 23:39:46 +0100

    Fix first build not having any CSS
    Tailwind is able to pick up changes faster since it doesn't need to wait
    for JS to be transpiled. But most importantly is the improved developer
    experience, avoiding a website without CSS the first time `npm run dev`
    is run.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  // in prod look at shadow-cljs output file in dev look at runtime, which will change files that are actually compiled; postcss watch should be a whole lot faster
-  content: process.env.NODE_ENV == 'production' ? ["./public/js/main.js"] : ["./public/js/cljs-runtime/*.js"],
+  content: [
+    "./src/main/**/*.cljs"
+  ],
   theme: {
     extend: {
       fontFamily: {
