665a6b84a953892064f7a4accab436cffba21056
Jacek Schae <jacek.schae@gmail.com>
2023-04-28 22:04:40 +0200

    Bump dependencies, include PR from @jacobemcken.
    Co-authored-by: Jacob Emcken <je@casalogic.dk>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   // in prod look at shadow-cljs output file in dev look at runtime, which will change files that are actually compiled; postcss watch should be a whole lot faster
-  content: process.env.NODE_ENV == 'production' ? ["./public/js/main.js"] : ["./public/js/cljs-runtime/*.js"],
+  content: process.env.NODE_ENV == 'production' ? ["./public/js/main.js"] : ["./src/main/**/*.cljs", "./public/js/cljs-runtime/*.js"],
   theme: {
     extend: {
       fontFamily: {
